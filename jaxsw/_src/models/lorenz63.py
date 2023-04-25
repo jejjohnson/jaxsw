7c4c0f94a321b5ebc850f58be1eae06cab856e14
Juan Emmanuel Johnson <jemanjohnson34@gmail.com>
2023-04-25 22:03:44 +0200

    remove rogue methods for l63

--- jaxsw/_src/models/lorenz63.py
+++ jaxsw/_src/models/lorenz63.py
@@ -94,20 +94,6 @@ class Lorenz63(DynamicalSystem):
     #     t = jnp.asarray([n * self.dt for n in range(n_steps)])
     #     return x[:: self.observe_every], t[:: self.observe_every]
 
-    def init_x0(self, noise: float = 0.01):
-        x0 = jnp.ones((3,))
-        perturb = noise * jrandom.normal(self.key, shape=())
-        return x0.at[0].set(x0[0] + perturb)
-
-    def init_x0_batch(self, batchsize: int, noise: float = 0.01):
-        # initial state (equilibrium)
-        x0 = jnp.ones((batchsize, 3))
-
-        # small perturbation
-        perturb = noise * jrandom.normal(self.key, shape=(batchsize,))
-
-        return x0.at[..., 0].set(x0[..., 0] + perturb)
-
 
 def rhs_lorenz_63(
     state: Tuple[Array, Array, Array],
