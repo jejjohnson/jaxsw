8766289aafeb56ae9837b169a4950903fb0c69dd
Juan Emmanuel Johnson <jemanjohnson34@gmail.com>
2023-06-15 00:32:19 +0200

    Updated Lorenz63/96 with new API.

--- jaxsw/_src/models/lorenz63.py
+++ jaxsw/_src/models/lorenz63.py
@@ -1,6 +1,6 @@
 from typing import NamedTuple, Optional, Tuple
 
-import diffrax as dfx
+import equinox as eqx
 import jax.numpy as jnp
 import jax.random as jrandom
 from equinox import static_field
@@ -8,6 +8,12 @@ from jax.random import PRNGKeyArray
 from jaxtyping import Array, PyTree
 
 from .base import DynamicalSystem
+
+
+class L63Params(eqx.Module):
+    s: Array = static_field()
+    r: Array = static_field()
+    b: Array = static_field()
 
 
 class L63State(NamedTuple):
@@ -19,89 +25,60 @@ class L63State(NamedTuple):
     def init_state(
         cls,
         noise: float = 0.01,
-        key: jrandom.PRNGKey = jrandom.PRNGKey(123),
+        s: float = 8,
+        r: float = 28,
+        b: float = 8.0 / 3.0,
+        key: PRNGKeyArray = jrandom.PRNGKey(123),
     ):
         x0, y0, z0 = jnp.ones((3,))
 
         perturb = noise * jrandom.normal(key, shape=())
 
-        return cls(x=x0 + perturb, y=y0, z=z0)
+        return cls(x=x0 + perturb, y=y0, z=z0), L63Params(s=s, r=r, b=b)
 
     @classmethod
     def init_state_batch(
         cls,
         batchsize: int = 10,
         noise: float = 0.01,
-        key: jrandom.PRNGKey = jrandom.PRNGKey(123),
+        s: float = 8,
+        r: float = 28,
+        b: float = 8.0 / 3.0,
+        key: PRNGKeyArray = jrandom.PRNGKey(123),
     ):
         x0, y0, z0 = jnp.array_split(jnp.ones((batchsize, 3)), 3, axis=-1)
 
         perturb = noise * jrandom.normal(key, shape=(batchsize, 1))
 
-        return cls(x=x0 + perturb, y=y0, z=z0)
-
-    @staticmethod
-    def update_state(state, **kwargs):
-        return L63State(
-            x=kwargs.get("x", state.x),
-            y=kwargs.get("y", state.y),
-            z=kwargs.get("z", state.z),
-        )
+        return cls(x=x0 + perturb, y=y0, z=z0), L63Params(s=s, r=r, b=b)
 
 
 class Lorenz63(DynamicalSystem):
-    # observe_every: int = static_field()
-    s: float = static_field()
-    r: float = static_field()
-    b: float = static_field()
-    key: jrandom.PRNGKey = jrandom.PRNGKey(0)
-
-    def __init__(
-        self,
-        tmin: float,
-        tmax: float,
-        s: float = 8,
-        r: float = 28,
-        b: float = 8.0 / 3.0,
-        key: PRNGKeyArray = jrandom.PRNGKey(0),
-        solver: dfx.AbstractSolver = dfx.Euler(),
-        stepsize_controller: dfx.PIDController = dfx.ConstantStepSize(),
-    ):
-        super().__init__(
-            tmin=tmin,
-            tmax=tmax,
-            solver=solver,
-            stepsize_controller=stepsize_controller,
-        )
-        self.s = s
-        self.r = r
-        self.b = b
-        # self.observe_every = observe_every
-        self.key = key
-
     def equation_of_motion(
         self, t: float, state: L63State, args: Optional[PyTree] = None
     ) -> L63State:
         # compute RHS
-        x_dot, y_dot, z_dot = rhs_lorenz_63(state=state, s=self.s, r=self.r, b=self.b)
+        x, y, z = state.x, state.y, state.z
+        s, r, b = args.s, args.r, args.b
+
+        x_dot, y_dot, z_dot = rhs_lorenz_63(x=x, y=y, z=z, s=s, r=r, b=b)
 
         # update state
-        state = state.update_state(state, x=x_dot, y=y_dot, z=z_dot)
+        state = eqx.tree_at(lambda u: u.x, state, x_dot)
+        state = eqx.tree_at(lambda u: u.y, state, y_dot)
+        state = eqx.tree_at(lambda u: u.z, state, z_dot)
 
         return state
 
-    # def observe(self, x: Float[Array, " dim"], n_steps: int):
-    #     t = jnp.asarray([n * self.dt for n in range(n_steps)])
-    #     return x[:: self.observe_every], t[:: self.observe_every]
-
 
 def rhs_lorenz_63(
-    state: Tuple[Array, Array, Array],
+    x: Array,
+    y: Array,
+    z: Array,
     s: float = 10,
     r: float = 28,
     b: float = 2.667,
 ) -> Tuple[Array, Array, Array]:
-    x, y, z = state
     x_dot = s * (y - x)
     y_dot = r * x - y - x * z
     z_dot = x * y - b * z
--- jaxsw/_src/models/lorenz96.py
+++ jaxsw/_src/models/lorenz96.py
@@ -1,6 +1,5 @@
-from typing import NamedTuple, Optional
+from typing import NamedTuple
 
-import diffrax as dfx
 import equinox as eqx
 import jax.numpy as jnp
 import jax.random as jrandom
@@ -12,7 +11,7 @@ from .base import DynamicalSystem
 
 
 class L96Params(eqx.Module):
-    F: float = static_field()
+    F: Array = static_field()
 
 
 class L96State(NamedTuple):
@@ -24,7 +23,7 @@ class L96State(NamedTuple):
         ndim: int = 10,
         F: float = 8,
         noise: float = 0.01,
-        key: jrandom.PRNGKey = jrandom.PRNGKey(123),
+        key: PRNGKeyArray = jrandom.PRNGKey(123),
     ):
         x0 = F * jnp.ones(shape=(ndim,))
 
@@ -32,7 +31,7 @@ class L96State(NamedTuple):
 
         x0 = x0.at[0].set(x0[0] + perturb)
 
-        return cls(x=x0), L96Params(F=F)
+        return cls(x=x0), L96Params(F=jnp.asarray(F))
 
     @classmethod
     def init_state_batch(
@@ -41,7 +40,7 @@ class L96State(NamedTuple):
         batchsize: int = 5,
         F: float = 8,
         noise: float = 0.01,
-        key: jrandom.PRNGKey = jrandom.PRNGKey(123),
+        key: PRNGKeyArray = jrandom.PRNGKey(123),
     ):
         x0 = F * jnp.ones(shape=(batchsize, ndim))
 
@@ -53,34 +52,17 @@ class L96State(NamedTuple):
 
 
 class Lorenz96(DynamicalSystem):
-    # observe_every: int = static_field()
-    key: jrandom.PRNGKey = jrandom.PRNGKey(0)
+    advection: bool
 
-    def __init__(
-        self,
-        tmin: float,
-        tmax: float,
-        key: PRNGKeyArray = jrandom.PRNGKey(0),
-        solver: dfx.AbstractSolver = dfx.Euler(),
-        stepsize_controller: dfx.PIDController = dfx.ConstantStepSize(),
-    ):
-        super().__init__(
-            tmin=tmin,
-            tmax=tmax,
-            solver=solver,
-            stepsize_controller=stepsize_controller,
-        )
-        # self.observe_every = observe_every
-        self.key = key
+    def __init__(self, advection: bool = True):
+        self.advection = advection
 
-    def equation_of_motion(
-        self, t: float, state: L96State, args: Optional[PyTree] = 8
-    ) -> L96State:
+    def equation_of_motion(self, t: float, state: L96State, args: PyTree) -> PyTree:
         # compute RHS
         x = state.x
         F = args.F
 
-        x_dot = rhs_lorenz_96(x=x, F=F)
+        x_dot = rhs_lorenz_96(x=x, F=F, advection=self.advection)
 
         # update state
         state = eqx.tree_at(lambda x: x.x, state, x_dot)
@@ -88,57 +70,12 @@ class Lorenz96(DynamicalSystem):
         return state
 
 
-# @pytc.treeclass
-# class Lorenz96(DynamicalSystem):
-#     grid_size: int = pytc.field(nondiff=True)
-#     observe_every: int = pytc.field(nondiff=True)
-#     F: float = pytc.field(nondiff=True)
-#     key: jrandom.PRNGKey = jrandom.PRNGKey(0)
-
-#     def __init__(
-#         self,
-#         dt: Float,
-#         grid_size: Int,
-#         observe_every: Int = 1,
-#         F: Int = 8,
-#         key: PyTree = jrandom.PRNGKey(0),
-#     ):
-#         self.dt = dt
-#         self.grid_size = grid_size
-#         self.observe_every = observe_every
-#         self.F = F
-#         self.key = key
-
-#     @property
-#     def state_dim(self):
-#         return (self.grid_size,)
-
-#     def equation_of_motion(
-#         self, x: Float[Array, " dim"], t: float
-#     ) -> Float[Array, " dim"]:
-#         return rhs_lorenz_96(x=x, t=t, F=self.F)
-
-#     def observe(self, x: Float[Array, " dim"], n_steps: int):
-#         t = jnp.asarray([n * self.dt for n in range(n_steps)])
-#         return x[:: self.observe_every], t[:: self.observe_every]
-
-#     def init_x0(self, noise: float = 0.01):
-#         x0 = self.F * jnp.ones(self.grid_size)
-#         perturb = noise * jrandom.normal(self.key, shape=())
-#         return x0.at[0].set(x0[0] + perturb)
-
-#     def init_x0_batch(self, batchsize: int, noise: float = 0.01):
-#         # initial state (equilibrium)
-#         x0 = self.F * jnp.ones((batchsize, self.grid_size))
-
-#         # small perturbation
-#         perturb = noise * jrandom.normal(self.key, shape=(batchsize,))
-
-#         return x0.at[..., 0].set(x0[..., 0] + perturb)
-
-
-def rhs_lorenz_96(x: Array, F: float = 8) -> Array:
+def rhs_lorenz_96(x: Array, F: float = 8, advection: bool = True) -> Array:
     x_plus_1 = jnp.roll(x, -1)
     x_minus_2 = jnp.roll(x, 2)
     x_minus_1 = jnp.roll(x, 1)
-    return (x_plus_1 - x_minus_2) * x_minus_1 - x + F
+
+    if advection:
+        return (x_plus_1 - x_minus_2) * x_minus_1 - x + F
+    else:
+        return -x + F
